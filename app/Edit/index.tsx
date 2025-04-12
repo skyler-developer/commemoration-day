b5e03e720a136fdc0055db9cc7f45c47684e23db
skyler <skylerhaut@foxmail.com>
2025-04-13 00:23:38 +0800

    style: 删除部分注释

--- app/Edit/index.tsx
+++ app/Edit/index.tsx
@@ -232,7 +232,6 @@ const styles = StyleSheet.create({
         marginLeft: 10,
         marginRight: 10,
         marginTop: 10,
-        // height: 80,
     },
     icon: {
         marginLeft: "auto",
